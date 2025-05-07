1f059b915e6f7d352684ac93f06295c1e3814d05
Enoch Bamidele <enochjulius18@gmail.com>
2025-05-07 09:02:19 +0100

    worked onm a few things

---
+++ the_web/script.js
@@ -0,0 +1,16 @@
+console.log("What's going on?");
+const noOfGoals = 0;
+const goalButton = document.getElementById("create-goal");
+
+// goalButton.onclick = alert("You have clicked this button!!!!!!!");
+
+goalButton.addEventListener("click", setNewGoal()) ;
+{
+    goalButton.innerText = "initializing! Let's get you started..";
+}
+
+function setNewGoal() {
+    const form = 0;
+    // 
+}
+
