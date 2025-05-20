f837ea08121eb49b0ecf11a6be5b3e536accbd95
Enoch Bamidele <enochjulius18@gmail.com>
2025-05-20 19:52:20 +0100

    Did very little today 😪

--- the_web/script.js
+++ the_web/script.js
@@ -4,13 +4,18 @@ const goalButton = document.getElementById("create-goal");
 
 // goalButton.onclick = alert("You have clicked this button!!!!!!!");
 
-goalButton.addEventListener("click", setNewGoal()) ;
-{
+goalButton.addEventListener("click", function() {
     goalButton.innerText = "initializing! Let's get you started..";
-}
-
-function setNewGoal() {
+    
     const form = 0;
-    // 
+    const docs = Document.getElementById("doc");
+   docs.innerHTML = `
+// <html>
+// <form action="">
+//   <label for="">Goal title<input type="text"></label>
+// </form>
+// </html>
+// `; 
 }
+);
 
